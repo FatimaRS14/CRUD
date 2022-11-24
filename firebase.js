0348415a8aae8cca86fc7d28798a3904d5581d4a
Fatima <faty.aome.rojas14@gmail.com>
2022-11-24 06:28:25 -0600

    Ahora si ya esta quedando-Firestore setup

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getFirestore, collection, addDoc  } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtQk_7KJnPocA1mr9anJFJ2hw5FUahItg",
+  authDomain: "crud-dc65b.firebaseapp.com",
+  projectId: "crud-dc65b",
+  storageBucket: "crud-dc65b.appspot.com",
+  messagingSenderId: "670561921046",
+  appId: "1:670561921046:web:7a02e1b163e975eec82d37"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore()
+
+export const saveTask = (title, description) => 
+   addDoc(collection(db, 'tasks'), {title, description});
+
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+  import {saveTask} from './firebase.js'
+  window.addEventListener('DOMContentLoaded', () =>{
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCtQk_7KJnPocA1mr9anJFJ2hw5FUahItg",
-    authDomain: "crud-dc65b.firebaseapp.com",
-    projectId: "crud-dc65b",
-    storageBucket: "crud-dc65b.appspot.com",
-    messagingSenderId: "670561921046",
-    appId: "1:670561921046:web:7a02e1b163e975eec82d37"
-  };
+  })
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  const taskForm = document.getElementById('task-form')
+
+  taskForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const title= taskForm['task-title']
+    const description = taskForm['task-description']
+
+    saveTask(title.value, description.value)
+
+    taskForm.reset()
+    
+  })
