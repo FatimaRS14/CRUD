18e191073c42d8032b3bf9f8d5987e5afbf39255
Fatima <faty.aome.rojas14@gmail.com>
2022-11-24 08:37:37 -0600

    Actualizar datos

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, deleteDoc, onSnapshot, doc, getDoc} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, onSnapshot, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,15 +17,18 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore()
+const db = getFirestore();
 
-export const saveTask = (title, description) => 
-   addDoc(collection(db, 'tasks'), {title, description});
+export const saveTask = (title,description) => 
+    addDoc(collection(db, 'tasks'), {title, description});
 
-   export const getTasks = () => getDocs(collection(db, 'tasks'))
+export const getTasks = () => getDocs(collection(db, 'tasks'))
 
-   export const onGetTasks = (callback) => onSnapshot(collection(db, 'tasks'), callback)
+export const onGetTasks = (callback) => onSnapshot(collection(db, 'tasks'), callback)
 
-   export const deleteTask = id =>  deleteDoc(doc(db, 'tasks', id))
+export const deleteTask = id =>  deleteDoc(doc(db, 'tasks', id))
 
-   export const getTask = id => getDoc(doc(db, 'tasks', id))
+export const getTask = id => getDoc(doc(db, 'tasks', id))
+
+export const updateTask = (id, newFields) => updateDoc(doc(db, 'tasks', id), newFields);
+
--- index.js
+++ index.js
@@ -1,48 +1,64 @@
-import {saveTask, getTasks, onGetTasks} from './firebase.js'
+import {saveTask, getTasks, onGetTasks, deleteTask, getTask, updateTask} from './firebase.js'
+const taskForm = document.getElementById('task-form')
+const tasksContainer = document.getElementById('tasks-container')
+let editStatus = false;
+let id = '';
 
-  const taskForm = document.getElementById('task-form')
-  const tasksContainer = document.getElementById('tasks-container')
-  window.addEventListener('DOMContentLoaded', async () =>{
-    //const querySnapshot = await getTasks()
+window.addEventListener('DOMContentLoaded', async () => {
     onGetTasks( (querySnapshot)=> {
-        let html = '';
+        tasksContainer.innerHTML = '';
     querySnapshot.forEach(doc => {
         const task = doc.data();
-        html += `
-        <div>
-            <h3>${task.title}</h3>
+        tasksContainer.innerHTML += `
+        <div class="card card-body mt-2 border-primary">
+            <h3 class="h5">${task.title}</h3>
             <p>${task.description}</p>
-            <button class="btn-delete" data-id="${doc.id}">Delete</button>
+            <div>
+                <button class="btn btn-outline-danger btn-delete" data-id="${doc.id}">Delete</button>
+                <button class="btn btn-outline-dark btn-edit" data-id="${doc.id}">Edit</button>
+            </div>
         </div>
         `;
     });
-
-    tasksContainer.innerHTML = html;
-
-    const btnsDelete = tasksContainer.querySelectorAll(".btn-delete");
-    btnsDelete.forEach((btn) =>
-      btn.addEventListener("click", async ({ target: { dataset } }) => {
-        try {
-          await deleteTask(dataset.id);
-        } catch (error) {
-          console.log(error);
-        }
-      })
-    );
     
+    const btnsDelete = tasksContainer.querySelectorAll('.btn-delete')
+    btnsDelete.forEach(btn => {
+        btn.addEventListener('click', ({target : {dataset}}) => {
+            deleteTask(dataset.id)
+        })
+    })
+
+    const btnsEdit = tasksContainer.querySelectorAll('.btn-edit')
+        btnsEdit.forEach(btn => {
+            btn.addEventListener('click', async(e) => {
+                const doc = await getTask(e.target.dataset.id)
+                const task = doc.data()
+
+                taskForm['task-title'].value = task.title
+                taskForm['task-description'].value = task.description
+
+                editStatus = true;
+                id = doc.id
+
+                taskForm['btn-task-save'].innerText = 'Update'
+            })
+        })
     });
+    
+})
 
-  });
-
-
-  taskForm.addEventListener('submit', (e) =>{
+taskForm.addEventListener('submit', (e) => {
     e.preventDefault()
-
-    const title= taskForm['task-title']
+    
+    const title = taskForm['task-title']
     const description = taskForm['task-description']
 
-    saveTask(title.value, description.value)
+    if (!editStatus){
+        saveTask(title.value, description.value);
+    } else {
+        updateTask(id,{title: title.value, description: description.value});
 
+        editStatus = false;
+    }
     taskForm.reset()
-
-  })
+});
