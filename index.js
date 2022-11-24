b3a870cd803bbbb1fa6e65f33fcdfc908b57bbc7
Fatima <faty.aome.rojas14@gmail.com>
2022-11-24 07:43:02 -0600

    Listar datos

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-  import {saveTask} from './firebase.js'
-  window.addEventListener('DOMContentLoaded', () =>{
-
-  })
+import {saveTask, getTasks, onGetTasks} from './firebase.js'
 
   const taskForm = document.getElementById('task-form')
+  const tasksContainer = document.getElementById('tasks-container')
+  window.addEventListener('DOMContentLoaded', async () =>{
+    //const querySnapshot = await getTasks()
+    onGetTasks( (querySnapshot)=> {
+        let html = '';
+    querySnapshot.forEach(doc => {
+        const task = doc.data();
+        html += `
+        <div>
+            <h3>${task.title}</h3>
+            <p>${task.description}</p>
+        </div>
+        `;
+    });
+
+    tasksContainer.innerHTML = html;
+    });
+
+  });
+
 
   taskForm.addEventListener('submit', (e) =>{
     e.preventDefault()
@@ -14,5 +31,5 @@
     saveTask(title.value, description.value)
 
     taskForm.reset()
-    
+
   })
